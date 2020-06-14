cabadd09d6c23524513ee2f70aac039a743b64bf
João <jgabrielafreitas@gmail.com>
2020-06-14 20:22:30 -0300

    add database config-common.conf

--- shared/src/main/kotlin/com/quick/tor/config/ApplicationConfig.kt
+++ shared/src/main/kotlin/com/quick/tor/config/ApplicationConfig.kt
@@ -1,8 +1,5 @@
 package com.quick.tor.config
 
-import com.typesafe.config.Config
-import java.util.*
-
 class ApplicationConfig(configRepository: ConfigRepository) {
 
     val config = configRepository.config.getConfig("app-config")
